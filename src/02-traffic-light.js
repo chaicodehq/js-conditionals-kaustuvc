f8883ecb5fd1730671a99e0f9fc25ed4c906f46a
kaustuvc <kaustuvchatterjee1234@gmail.com>
2026-02-08 21:08:56 +0530

    chore: solve 02-traffic-light.js

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -22,5 +22,24 @@
  * @returns {string} The driving action to take
  */
 export function getTrafficAction(color) {
-  // Your code here
+  color = color.toLowerCase();
+  let action;
+  switch (color) {
+    case "green":
+      action = "GO";
+      break;
+    case "yellow":
+      action = "SLOW DOWN";
+      break;
+    case "red":
+      action = "STOP";
+      break;
+      case "flashing red":
+      action = "STOP AND PROCEED WITH CAUTION";
+      break;
+    default:
+      action = "INVALID SIGNAL";
+      break;
+  }
+  return action;
 }
